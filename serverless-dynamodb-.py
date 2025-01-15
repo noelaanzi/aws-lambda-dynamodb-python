3c54e981501a41c40f3c8c335aa1051a2c6af5db
Arockiadoss Jesudoss <noelaanzi@gmail.com>
2025-01-15 22:02:21 +0900

    first commit

---
+++ serverless-dynamodb-.py
@@ -0,0 +1,81 @@
+import json
+import boto3
+from botocore.exceptions import ClientError
+
+
+dynamodb = boto3.resource("dynamodb", region_name='ap-northeast-1')
+table = dynamodb.Table('customer')
+
+
+def lambda_handler(event, context):
+    # Extract query parameters from the event object
+    # Get HTTP method and resource path
+    try:
+        http_method = event.get("httpMethod")
+        resource = event.get("resource")
+        responseHeader = {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",  # Replace with your domain for production
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        }
+
+        # Handle different routes
+        responseBody = {}
+        if http_method == "GET":
+            # Example: Fetch list of users
+            customerId = event.get("queryStringParameters", {}).get(
+                "CustomerId", "No Value"
+            )
+
+            resultSet = table.get_item(
+                Key={'CustomerId': customerId})
+            dataSet = resultSet["Item"]
+            responseBody = [
+                {'CustomerId': dataSet['CustomerId'], 'Name': dataSet['Name'], 'Email': dataSet['Email']}]
+
+            # Process the query parameter
+            # response_message = f"Received query parameter: {query_param}"
+            print(responseBody)
+            # Return a response with CORS headers
+
+            return {
+                "statusCode": 200,
+                "headers": responseHeader,
+                "body": json.dumps({"message": responseBody})
+            }
+
+        elif http_method == 'POST':
+            # Parse request body
+            body = json.loads(event['body'])
+            print('body is')
+            print(body)
+            customerId = body.get('customerId')
+            name = body.get('name')
+            email = body.get('email')
+            if not customerId or not name or not email:
+                return {
+                    "statusCode": 400,
+                    "headers": responseHeader,
+                    "body": json.dumps({"error": "CustomerID, Name,Email Required."})
+                }
+
+            # Insert item into DynamoDB
+            table.put_item(Item={"CustomerId": customerId, "Name": name, "Email": email})
+            return {
+                "statusCode": 200,
+                "headers": responseHeader,
+                "body": json.dumps({"message": "1 Item Inserted successfully!"})
+            }
+        else:
+            return {
+                "statusCode": 405,
+                "headers": responseHeader,
+                "body": json.dumps({"error": "Method not allowed"})
+            }
+
+    except ClientError as e:
+        return {
+            "statusCode": 500,
+            "headers": responseHeader,
+            "body": json.dumps({"error": str(e)})
+        }
